4334daa25b654add10b4384a41a8d3ba6743cebc
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-08-25 09:15:33 +0200

    Production goes live
    New feature was removed

--- GitKraken.py
+++ GitKraken.py
@@ -6,3 +6,5 @@ st.write("Main_branch")
 st.write("In the meantime the PRODUCTION branch is being deployed to the website")
 
 st.write("A bug fix to the PRODUCTION branch")
+
+st.write("PRODUCTION is fixed again. Let's finally deply live.")
