8642db4efcb1f650122034ee6cb93cf3bbf7e408
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-08-25 09:20:00 +0200

    Bug fix on PRODUCTION

--- GitKraken.py
+++ GitKraken.py
@@ -7,4 +7,4 @@ st.write("In the meantime the PRODUCTION branch is being deployed to the website
 
 st.write("A bug fix to the PRODUCTION branch")
 
-st.write("PRODUCTION is fixed again. Let's finally deply live.")
+st.write("PRODUCTION is fixed again. Let's finally deploy live.")
