def4192a0e63d09c9b70def2850af70b7ee2f64d
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-08-25 10:19:08 +0200

    Pimp up PRODUCTION

--- GitKraken.py
+++ GitKraken.py
@@ -8,3 +8,5 @@ st.write("In the meantime the PRODUCTION branch is being deployed to the website
 st.write("A bug fix to the PRODUCTION branch")
 
 st.write("PRODUCTION is fixed again. Let's finally deploy live.")
+
+st.write("PRODUCTION branch gets a revamp!")
